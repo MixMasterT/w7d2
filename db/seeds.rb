6d8c0a259b06c29435322c2790368da462db0a14
MixMasterT <torahlight@gmail.com>
2017-01-03 17:27:34 -0800

    Set up rails app with React frontend, not yet connected

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(title: "make money", body: "but work hard", done: false)
+Todo.create(title: "take money", body: "quickly", done: true)
+Todo.create(title: "finish", body: "don't think too much", done: true)
+Todo.create(title: "sleep", body: "i feel so tired", done: false)
+Todo.create(title: "eat", body: "food is delicious", done: true)
